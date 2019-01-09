648abafccd702865bb2568c80dbdeafe58c7d12f
vasiliyaltunin <skyr@inbox.ru>
2019-01-09 17:37:58 +1100

    Initial import

---
+++ README.md
@@ -0,0 +1,39 @@
+# Robots.txt Gin's middleware
+
+Gin middleware to support robots.txt.
+
+based on https://github.com/thinkerou/favicon
+
+## Usage
+
+### Start using it
+
+Download and install it:
+
+```sh
+$ go get github.com/vasiliyaltunin/gorobots
+```
+
+Import it in your code:
+
+```go
+import "github.com/vasiliyaltunin/gorobots"
+```
+
+### Canonical example:
+
+```go
+package main
+            
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/vasiliyaltunin/gorobots"
+)
+            
+func main() {
+    r := gin.Default()
+    r.Use(gorobot.New("./static/robots.txt")) // set gorobots middleware 
+
+    r.Run(":8080")
+}
+```
---
+++ robots.go
@@ -0,0 +1,52 @@
+package gorobots
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+func New(path string) gin.HandlerFunc {
+	path = filepath.FromSlash(path)
+	if len(path) > 0 && !os.IsPathSeparator(path[0]) {
+		wd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		path = filepath.Join(wd, path)
+	}
+
+	info, err := os.Stat(path)
+	if err != nil || info == nil || info.IsDir() {
+		panic("Invalid favicon path: " + path)
+	}
+
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bytes.NewReader(file)
+
+	return func(c *gin.Context) {
+		if c.Request.RequestURI != "/robots.txt" {
+			return
+		}
+		if c.Request.Method != "GET" && c.Request.Method != "HEAD" {
+			status := http.StatusOK
+			if c.Request.Method != "OPTIONS" {
+				status = http.StatusMethodNotAllowed
+			}
+			c.Header("Allow", "GET,HEAD,OPTIONS")
+			c.AbortWithStatus(status)
+			return
+		}
+		c.Header("Content-Type", "text/plain")
+		http.ServeContent(c.Writer, c.Request, "robots.txt", info.ModTime(), reader)
+		return
+	}
+}
